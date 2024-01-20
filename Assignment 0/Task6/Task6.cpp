680877123cecbf1b525ab22ad24aa5233d3a0cbf
h598062 <598062@stud.hvl.no>
2024-01-20 15:34:52 +0100

    task 6

---
+++ Assignment 0/Task6/Task6.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef vector<wstring> Words;
+
+int main(int argc, char* argv[])
+{
+    wcout << L"Please write words to be sorted, one word per line. Send an empty line to stop." << L"\n";
+	Words words;
+    while (true)
+    {
+    	wstring input;
+    	getline(wcin, input);
+	    if (input.empty())
+	    {
+		    break;
+	    }
+    	// wcout << L"You sent: " << input << L"\n";
+	    words.push_back(input);
+    }
+
+    sort(words.begin(), words.end());
+	wcout << L"Sorted alpabetically:\n";
+	for (vector<wstring>::iterator i = words.begin(); i < words.end(); ++i)
+	{
+		wcout << *i << L"\n";
+	}
+
+	return 0;
+}
