39a402eb8295277d4356e2091b5018e63560a82c
Bjørnar <598062@stud.hvl.no>
2024-02-01 14:24:08 +0100

    Update Task8.cpp

--- Assignment 0/Task8/Task8.cpp
+++ Assignment 0/Task8/Task8.cpp
@@ -74,7 +74,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TASK8));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hCursor        = LoadCursor(nullptr, IDC_HAND);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TASK8);
     wcex.lpszClassName  = szWindowClass;
