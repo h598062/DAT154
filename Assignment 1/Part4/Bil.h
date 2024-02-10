a0f8a239e1eee9273590cd4802fc69831dd3c97a
h598062 <598062@stud.hvl.no>
2024-02-10 14:54:39 +0100

    header files v1

---
+++ Assignment 1/Part4/Bil.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "framework.h"
+
+enum class BilColour
+{
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW,
+	BLACK,
+	WHITE
+};
+
+enum class BilDirection
+{
+	HORIZONTAL,
+	VERTICAL
+};
+
+class Bil
+{
+	BilColour colour;
+
+	HBRUSH redBrush;
+	HBRUSH yellowBrush;
+	HBRUSH greenBrush;
+	HBRUSH blueBrush;
+	HBRUSH blackBrush;
+	HBRUSH whiteBrush;
+
+public:
+	int size;
+	BilDirection dir;
+	int pos;
+	bool invalid;
+
+	Bil(const BilColour colour, const BilDirection dir, const int size);
+
+	void updatePos(const int movement);
+	void draw(const HDC& hdc, const int line) const;
+	bool collidesWith(const int otherPos, const int movesize) const;
+};
---
+++ Assignment 1/Part4/TrafficLight.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+
+#include "framework.h"
+
+enum class TLState
+{
+	STOP,
+	READY,
+	GO,
+	SLOW
+};
+
+enum class TLDir
+{
+	VERTICAL,
+	VERTICALFLIPPED,
+	HORIZONTAL,
+	HORIZONTALFLIPPED
+};
+
+class TLCircle
+{
+public:
+	int top;
+	int bottom;
+	int left;
+	int right;
+
+	int size;
+
+	TLCircle(const int size) :
+		top(0),
+		bottom(size),
+		left(0),
+		right(size),
+		size(size) {}
+
+	void draw(const HDC hdc) const;
+	void setPos(const int x, const int y);
+};
+
+class TrafficLight
+{
+	TLState state;
+	int posX;
+	int posY;
+	TLDir dir;
+	const int spacing = 10;
+	const int circleSize = 50;
+
+	TLCircle* red;
+	TLCircle* yellow;
+	TLCircle* green;
+	HBRUSH offBrush;
+	HBRUSH redBrush;
+	HBRUSH yellowBrush;
+	HBRUSH greenBrush;
+	HBRUSH bgBrush;
+	HPEN tlOutlinePen;
+
+	void drawLights(const HDC& hdc, const bool redState, const bool yellowState, const bool greenState) const;
+	void drawTLBox(const HDC& hdc) const;
+
+public:
+	int stopPos;
+	TrafficLight(const TLState state, const TLDir dir, const int stopPos);
+	void setPos(const int x, const int y);
+	void changeState();
+	void draw(const HDC& hdc) const;
+	bool canDrive() const;
+	void cleanUp();
+};
+
