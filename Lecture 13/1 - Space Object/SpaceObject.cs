cdcfe454d817dca06558369215e62ae0b8f8aa74
Bjørnar <598062@stud.hvl.no>
2024-02-19 11:27:36 +0100

    lecture 13

---
+++ Lecture 13/1 - Space Object/SpaceObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SpaceSim {
+    public class SpaceObject {
+
+        protected String name;
+
+        public SpaceObject(String name) {
+            this.name = name;
+        }
+        public virtual void Draw() {
+            Console.WriteLine(name);
+        }
+    }
+
+    public class Star : SpaceObject {
+        public Star(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Star  : ");
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject {
+        public Planet(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+    }
+
+    public class Moon : SpaceObject {
+        public Moon(String name) : base(name) { }
+
+        public override void Draw() {
+            Console.Write("Moon  : ");
+            base.Draw();
+        }
+    }
+}
+
---
+++ Lecture 13/2 - Space Sim/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceSim;
+
+class Astronomy {
+    public static void Main() {
+        List<SpaceObject> a = new List<SpaceObject>
+        {
+            new Star("Sun"),
+            new Planet("Mercury"),
+            new Planet("Venus"),
+            new Planet("Terra"),
+            new Moon("The Moon")
+        };
+
+        foreach (var s in a) {
+            s.Draw();
+        }
+
+        Console.ReadLine();
+    }
+}
+
