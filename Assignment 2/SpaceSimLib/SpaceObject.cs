ef9a209fa22a5cbe35c96ea9030a495ff513ff42
h598062 <598062@stud.hvl.no>
2024-03-04 16:05:37 +0100

    task 1 2 3, delvis 4

--- Assignment 2/SpaceSimLib/SpaceObject.cs
+++ Assignment 2/SpaceSimLib/SpaceObject.cs
@@ -1,32 +1,154 @@
-﻿using System;
-namespace SpaceSimLib {
-    public class SpaceObject {
-        protected String name;
-        public SpaceObject(String name) {
-            this.name = name;
+﻿using System.Numerics;
+
+namespace SpaceSimLib
+{
+    /// <summary>
+    /// An object in space.
+    /// </summary>
+    public abstract class SpaceObject
+    {
+        public String Name { get; }
+        public long DiameterKm { get; }
+        public string Colour { get; }
+
+        protected SpaceObject(String name, long diameterKm, String colour)
+        {
+            this.Name = name;
+            this.DiameterKm = diameterKm;
+            this.Colour = colour;
         }
-        public virtual void Draw() {
-            Console.WriteLine(name);
+
+        public virtual void Draw()
+        {
+            Console.WriteLine($"{Name} ({DiameterKm} km) - {Colour}");
+        }
+        
+    }
+
+
+    /// <summary>
+    /// Any object that orbits another object.
+    /// </summary>
+    public abstract class OrbitalBody : SpaceObject
+    {
+        public SpaceObject Orbiting { get; }
+        public float OrbitalPeriodDays { get; }
+
+        /// <summary>
+        /// Orbital radius in thousands of kilometers. 5 = 5000 km.
+        /// </summary>
+        public long OrbitalRadiuskKm { get; }
+
+        protected OrbitalBody(String name, long diameterKm, String colour,
+            SpaceObject orbiting, float orbitalPeriodDays, long orbitalRadiuskKm) :
+            base(name, diameterKm, colour)
+        {
+            this.Orbiting = orbiting;
+            this.OrbitalPeriodDays = orbitalPeriodDays;
+            this.OrbitalRadiuskKm = orbitalRadiuskKm;
+        }
+
+        public Vector2 GetPosition(float timeDays)
+        {
+            float angle = timeDays / OrbitalPeriodDays * 2 * (float)Math.PI;
+            return new Vector2((float)Math.Cos(angle) * OrbitalRadiuskKm,
+                (float)Math.Sin(angle) * OrbitalRadiuskKm);
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            Console.Write(" - Orbiting: ");
+            Orbiting.Draw();
         }
     }
-    public class Star : SpaceObject {
-        public Star(String name) : base(name) { }
-        public override void Draw() {
-            Console.Write("Star : ");
+
+    /// <summary>
+    /// The central object in a solar system.
+    /// </summary>
+    public class Star : SpaceObject
+    {
+        public Star(String name, long diameterKm, String colour) :
+            base(name, diameterKm, colour)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Star: ");
             base.Draw();
         }
     }
-    public class Planet : SpaceObject {
-        public Planet(String name) : base(name) { }
-        public override void Draw() {
+
+    /// <summary>
+    /// A large Solar System Body that orbits a Star.
+    /// </summary>
+    public class Planet : OrbitalBody
+    {
+        public Planet(String name, long diameterKm, String colour, Star orbiting,
+            float orbitalPeriodDays, long orbitalRadiuskKm) :
+            base(name, diameterKm, colour, orbiting, orbitalPeriodDays, orbitalRadiuskKm)
+        {
+        }
+
+        public override void Draw()
+        {
             Console.Write("Planet: ");
             base.Draw();
         }
     }
-    public class Moon : Planet {
-        public Moon(String name) : base(name) { }
-        public override void Draw() {
-            Console.Write("Moon : ");
+
+    /// <summary>
+    /// A natural satellite orbiting a planet.
+    /// </summary>
+    public class Moon : OrbitalBody
+    {
+        public Moon(String name, long diameterKm, String colour, Planet orbiting,
+            float orbitalPeriodDays, long orbitalRadiuskKm) :
+            base(name, diameterKm, colour, orbiting, orbitalPeriodDays, orbitalRadiuskKm)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Moon: ");
+            base.Draw();
+        }
+    }
+
+    /// <summary>
+    /// Planet, but smaller
+    /// </summary>
+    public class DwarfPlanet : OrbitalBody
+    {
+        public DwarfPlanet(String name, long diameterKm, String colour,
+            Star orbiting, float orbitalPeriodDays, long orbitalRadiuskKm) :
+            base(name, diameterKm, colour, orbiting, orbitalPeriodDays, orbitalRadiuskKm)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Dwarf Planet: ");
+            base.Draw();
+        }
+    }
+
+
+    /// <summary>
+    /// An SSSB that mostly consists of rock and metal.
+    /// </summary>
+    public class Asteroid : OrbitalBody
+    {
+        public Asteroid(String name, long diameterKm, String colour, Star orbiting,
+            float orbitalPeriodDays, long orbitalRadiuskKm) :
+            base(name, diameterKm, colour, orbiting, orbitalPeriodDays, orbitalRadiuskKm)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroid: ");
             base.Draw();
         }
     }
--- Assignment 2/SpaceSimLibTest/Program.cs
+++ Assignment 2/SpaceSimLibTest/Program.cs
@@ -1,24 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using SpaceSimLib;
+﻿using SpaceSimLib;
 
 class SpaceSimLibTest
 {
     public static void Main()
     {
-        List<SpaceObject> solarSystem = new List<SpaceObject>
-        {
-            new Star("Sun"),
-            new Planet("Mercury"),
-            new Planet("Venus"),
-            new Planet("Terra"),
-            new Moon("The Moon")
-        };
+        List<SpaceObject> solarSystem = new List<SpaceObject>();
+        Star sun = new Star("The Sun", 1392000, "Yellow");
+
+        Planet mercury = new Planet("Mercury", 4879, "Grey", sun, 87.97f, 57910);
+
+        Planet venus = new Planet("Venus", 12104, "Yellow", sun, 224.7f, 108200);
+
+        Planet earth = new Planet("Earth", 12756, "Blue", sun, 365.26f, 149600);
+        Moon moon = new Moon("The Moon", 3475, "Grey", earth, 27.32f, 384);
+
+        Planet mars = new Planet("Mars", 6792, "Red", sun, 686.98f, 227940);
+        Moon phobos = new Moon("Phobos", 22, "Grey", mars, 0.32f, 9);
+        Moon deimos = new Moon("Deimos", 12, "Grey", mars, 1.26f, 23);
+
+        Planet jupiter = new Planet("Jupiter", 142984, "Brown", sun, 4332.71f, 778330);
+        Moon io = new Moon("Io", 3643, "Yellow", jupiter, 1.77f, 422);
+        Moon europa = new Moon("Europa", 3122, "White", jupiter, 3.55f, 671);
+        Moon ganymede = new Moon("Ganymede", 5262, "White", jupiter, 7.15f, 1070);
+        Moon callisto = new Moon("Callisto", 4821, "White", jupiter, 16.69f, 1883);
+
+        Planet saturn = new Planet("Saturn", 120536, "Brown", sun, 10759.5f, 1429400);
+        Moon titan = new Moon("Titan", 5150, "Brown", saturn, 15.95f, 1221);
+        Moon phoebe = new Moon("Phoebe", 220, "Brown", saturn, 550.48f, 12952);
+
+        Planet uranus = new Planet("Uranus", 51118, "Blue", sun, 30685f, 2870990);
+
+        Planet neptune = new Planet("Neptune", 49528, "Blue", sun, 60190f, 4504300);
+
+        DwarfPlanet eris = new DwarfPlanet("Eris", 2326, "Brown", sun, 203830, 149597);
+        DwarfPlanet ceres = new DwarfPlanet("Ceres", 945, "Brown", sun, 1680, 413700);
+        DwarfPlanet pluto = new DwarfPlanet("Pluto", 2370, "Brown", sun, 90520, 5913520);
+
+
+        Asteroid pallas = new Asteroid("Pallas", 544, "Grey", sun, 1680, 413700);
+
+
+        solarSystem.Add(sun);
+        solarSystem.Add(mercury);
+        solarSystem.Add(venus);
+        solarSystem.Add(earth);
+        solarSystem.Add(moon);
+        solarSystem.Add(mars);
+        solarSystem.Add(phobos);
+        solarSystem.Add(deimos);
+        solarSystem.Add(jupiter);
+        solarSystem.Add(io);
+        solarSystem.Add(europa);
+        solarSystem.Add(ganymede);
+        solarSystem.Add(callisto);
+        solarSystem.Add(saturn);
+        solarSystem.Add(titan);
+        solarSystem.Add(phoebe);
+        solarSystem.Add(uranus);
+        solarSystem.Add(neptune);
+        solarSystem.Add(ceres);
+        solarSystem.Add(pluto);
+        solarSystem.Add(eris);
+        solarSystem.Add(pallas);
+
+
         foreach (SpaceObject obj in solarSystem)
         {
             obj.Draw();
+            if (obj is OrbitalBody orbitalBody)
+            {
+                Console.WriteLine($"{orbitalBody.Name} Position at 20 days: {orbitalBody.GetPosition(20)}");
+            }
         }
-
-        Console.ReadLine();
     }
 }
