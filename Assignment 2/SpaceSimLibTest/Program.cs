027a01b189676d518f95958d3f93e3f0c40e9d90
h598062 <598062@stud.hvl.no>
2024-03-04 11:35:11 +0100

    lib test

---
+++ Assignment 2/SpaceSimLibTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceSimLib;
+
+class SpaceSimLibTest
+{
+    public static void Main()
+    {
+        List<SpaceObject> solarSystem = new List<SpaceObject>
+        {
+            new Star("Sun"),
+            new Planet("Mercury"),
+            new Planet("Venus"),
+            new Planet("Terra"),
+            new Moon("The Moon")
+        };
+        foreach (SpaceObject obj in solarSystem)
+        {
+            obj.Draw();
+        }
+
+        Console.ReadLine();
+    }
+}
