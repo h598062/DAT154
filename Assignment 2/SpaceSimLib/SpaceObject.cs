724ce80d57bf689750ccdf3522bbcd4c23625094
h598062 <598062@stud.hvl.no>
2024-03-04 11:35:03 +0100

    lib

---
+++ Assignment 2/SpaceSimLib/SpaceObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace SpaceSimLib {
+    public class SpaceObject {
+        protected String name;
+        public SpaceObject(String name) {
+            this.name = name;
+        }
+        public virtual void Draw() {
+            Console.WriteLine(name);
+        }
+    }
+    public class Star : SpaceObject {
+        public Star(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Star : ");
+            base.Draw();
+        }
+    }
+    public class Planet : SpaceObject {
+        public Planet(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+    }
+    public class Moon : Planet {
+        public Moon(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Moon : ");
+            base.Draw();
+        }
+    }
+}
