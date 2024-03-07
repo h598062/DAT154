2913465916df1cec20f9514789d1e93edd440a3d
Bjørnar <598062@stud.hvl.no>
2024-03-07 15:53:34 +0100

    hente sol fra solsystem

--- Assignment 2/SpaceSimGUI/MainWindow.xaml.cs
+++ Assignment 2/SpaceSimGUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SpaceSimLib;
 
 namespace SpaceSimGUI;
 
@@ -16,24 +17,34 @@ namespace SpaceSimGUI;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private Solarsystem _solarsystem;
+    
+    private List<Ellipse> _ellipses = [];
+    
     public MainWindow()
     {
         InitializeComponent();
+        _solarsystem = new Solarsystem();
     }
 
     private void StartKnapp_OnClick(object sender, RoutedEventArgs e)
     {
         StartKnapp.Visibility = Visibility.Hidden;
         Console.WriteLine("Wallah");
-        Ellipse eclipse = new Ellipse
+        
+        Ellipse sun = new Ellipse
         {
+            Name = _solarsystem.Sun.Name.Replace(" ", ""),
             Width = 100,
             Height = 100,
-            Fill = Brushes.Red,
+            Fill = Brushes.Yellow,
             Margin = new Thickness(1),
             Stroke = Brushes.Black,
-            ToolTip = "This is a planet!"
+            ToolTip = _solarsystem.Sun.Name
         };
-        SimCanvas.Children.Add(eclipse);
+        SimCanvas.Children.Add(sun);
+        _ellipses.Add(sun);
+        Canvas.SetLeft(sun, _solarsystem.Sun.GetPosition(0).X - sun.Width / 2 + SimCanvas.RenderSize.Width / 2);
+        Canvas.SetTop(sun, _solarsystem.Sun.GetPosition(0).Y - sun.Height / 2 + SimCanvas.RenderSize.Height / 2);
     }
 }
