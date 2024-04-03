dd680be0515341426729ffa6904cd23d24099960
h598062 <598062@stud.hvl.no>
2024-04-03 18:13:32 +0200

    initial project(s)

---
+++ Assignment 3/ASPNET_WEBAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASPNET_WEBAPP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
